ef1ef3879368877275302e953113d5808fa7a12e
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2021-12-06 16:00:58 +0400

    kitabApp

--- kitabxana/settings.py
+++ kitabxana/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=!q*nrvv3(g=_hd$irh@0*lok#sohhtopnbu0+c&om&mc2_^q='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
